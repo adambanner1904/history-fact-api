3ca175f27733a42e2bcc1b0c7b47de5349d031a6
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2023-01-27 11:39:46 +0000

    Made more work on the webpages. Linked a form to go from index.html to data.html and to be able to retrieve the data. Next step is to work on the back end and retrieve the data from the API and rework it.

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ def home():
     return render_template("index.html", content="Testing")
 
 
-@app.route("/form1")
-def form():
-    return render_template("form1.html")
+@app.route("/data/", methods=['POST', "GET"])
+def data():
+    if request.method == "GET":
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == "POST":
+        form_data = request.form
+        for key, value in form_data:
+
+        return render_template("data.html", form_data=form_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
